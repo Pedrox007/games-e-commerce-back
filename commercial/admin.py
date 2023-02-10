33da1ed89556df6e9a28bd573d3c9208f38a82d3
Pedro Vicente <pedrov.nana@gmail.com>
2023-02-10 09:44:32 -0300

    Fix problem envolving product images url and admin list

--- commercial/admin.py
+++ commercial/admin.py
@@ -2,8 +2,27 @@ from django.contrib import admin
 
 from commercial.models import Product, Cart, CartItem, Order, OrderItem
 
-admin.site.register(Product)
-admin.site.register(Cart)
-admin.site.register(CartItem)
-admin.site.register(Order)
-admin.site.register(OrderItem)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "score")
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ("id", "user")
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "product", "quantity")
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user")
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "product", "quantity", "freight")
--- games_e_commerce/settings.py
+++ games_e_commerce/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 import environ
 
@@ -139,7 +139,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_URL = "/media/"
+MEDIA_URL = os.path.join(BASE_DIR, 'media/')
 MEDIA_ROOT = "media"
 
 # Default primary key field type
